15299439bb54e0dbdb8f81a1f75dd7c56ee5e34b
ssoney02 <ssoney0217@gmail.com>
2025-02-27 17:51:43 +0900

    stack

--- problems/feb26/21618.py
+++ problems/feb26/21618.py
@@ -1,7 +1,7 @@
 T = int(input())
 for tc in range(1, T+1):
     text = list(input())
-    stack = [0] * 100
+    stack = [0] * 100000    # 문자열 길이: 1000이내............
     # t를 돌면서 stack[top]이랑 계속 비교
     # 일치하면 top -= 1
     # 아니면 top += 1 stack[top] = t
