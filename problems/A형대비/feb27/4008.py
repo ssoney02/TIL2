2e6565daf50c3a39a35db52fb71e5b29220cca07
ssoney02 <ssoney0217@gmail.com>
2025-02-28 17:56:27 +0900

    0228

--- problems/A형대비/feb27/4008.py
+++ problems/A형대비/feb27/4008.py
@@ -1,20 +1,49 @@
-# def perm(cnt):
+from itertools import permutations
+import copy
+import math
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())    # 숫자의 개수
     counts = list(map(int, input().split()))   # 순서대로 +,-,*,/ 개수
     numbers = list(map(int, input().split()))
-    top1 = -1
-    top2 = 0
-    while top1 <= len(numbers):
-        top1 += 1
-        op1 = numbers[top1]
-        top1 += 1
-        op2 = numbers[top1]
-        # while top2 <= len(counts):
-        # top 확인은 한 번만 해야
-            if counts[top2] != 0:됨
-                counts[top2] -= 1
-            else:
-                top2 += 1
+    # copy_numbers = copy.deepcopy(numbers)
+    n_counts = []
+    for i in range(len(counts)):
+        for j in range(counts[i]):
+            n_counts.append(i)
+    # print(n_counts)
+
+    unique_permutations = set(permutations(n_counts))  # 그냥 가능한 경우 다 구해놓고 중복 제거!
+    ans = []
+    for perm in unique_permutations:        # 연산자 조합 하나씩 돌면서
+        # print(perm)
+        top1 = -1
+        top2 = 0
+        copy_numbers = copy.deepcopy(numbers)
+        # print(copy_numbers)
+        while top1 <= len(copy_numbers)-3:
+            top1 += 1
+            op1 = copy_numbers[top1]
+            top1 += 1
+            op2 = copy_numbers[top1]
+            if op2 == 0:
+                break
+
+            if perm[top2] == 0:
+                copy_numbers[top1] = op1 + op2
+            elif perm[top2] == 1:
+                copy_numbers[top1] = op1 - op2
+            elif perm[top2] == 2:
+                copy_numbers[top1] = op1 * op2
+            elif perm[top2] == 3:
+                copy_numbers[top1] = math.trunc(op1 / op2)
+            top2 += 1
+            top1 -= 1
+        ans.append(copy_numbers[-1])
+
+
+    # print(ans)
+    # print(max(ans))
+    # print(min(ans))
+    print(f'#{tc} {max(ans)-min(ans)}')
