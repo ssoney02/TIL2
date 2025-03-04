4cba6fba9121db1ed79be6f962ffc02a9c0c08ae
ssoney02 <ssoney0217@gmail.com>
2025-03-04 17:47:06 +0900

    0304

--- problems/A형대비/mar03/2105.py
+++ problems/A형대비/mar03/2105.py
@@ -1,25 +1,15 @@
 delta = [[1, 1], [1, -1], [-1, -1], [-1, 1]]
 # 부딪히면 방향 탐색하는게 아니라 매번 네방향 다 탐색해보고 진행하면서 init_i,init_j에 다시 도달하면
 # 거리 max 값 출력 !
-def find_route(depth, oi, oj, cnt):
-    # 종료조건: 네 방향으로 다 이동했고(depth == 4 방향 틀때마다 depth+1넘김), init_i, init_j
-    # 델타 시계방향으로 탐색
-    # 이동할 수 있으면 이동( oi+delta[k] visited == 0
-    # 아니면 델타 반시계 방향으로 탐색
 
-    for k in range(4):
-        new_i, new_j = oi + delta[k], oj+delta[k]
-        if (0<=new_i<N) and (0<=new_j<N) and (cafe[new_i][new_j] not in check_dessert) and visited[new_i][new_j] == 0:
-            cnt += 1
-            visited[new_i][new_j] = 1       # 방문체
-            check_dessert.append(cafe[oi][oj])
-            find_route(depth+1, new_i, new_j)
-            cnt -= 1
-            visited[new_i][new_j] = 0
-            check_dessert.pop()
+def find_route(k, oi, oj, cnt):
+    global max_cnt
 
 
-
+    # 한 방향씩 잡고 이동할 것
+    # 한 방향으로 쭉 이동하다가 더 이상 갈 곳이 없으면 return 하고 나와서 다른 방향 탐색
+    ni, nj = oi + delta[k], oj + delta[k]
+    if 0 <= ni < N and 0 <= nj < N and visited[ni][nj] == 0:
 
 
 
@@ -30,7 +20,16 @@ for tc in range(1, T+1):
     cafe = [list(map(int, input().split())) for _ in range(N)]
     visited = [[0]*N for _ in range(N)]
     check_dessert = []
+    tmp_len = []
+    max_cnt, max_result = -float('inf'), -float('inf')
+    result = -1
     for i in range(N):
         for j in range(N):
-            if [i,j] not in [[0,0], [0, N-1], [N-1, 0], [N-1, N-1]]:
-                find_route()
+            if [i, j] not in [[0,0], [0, N-1], [N-1, 0], [N-1, N-1]]:
+                si, sj = i, j
+                find_route(0, i, j, 0)
+                # 시작점 별 max값이 max_cnt
+                # 케이스 하나 당 max값은 max_result
+                max_result = max(max_result, max_cnt)
+
+    print(f'#{tc} {max_result}')
