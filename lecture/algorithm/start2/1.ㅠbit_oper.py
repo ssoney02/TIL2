dc53b2046492882a12ec870dd598c456665baa21
ssoney02 <ssoney0217@gmail.com>
2025-03-07 09:31:57 +0900

    bit

---
+++ lecture/algorithm/start2/1.ㅠbit_oper.py
@@ -0,0 +1,12 @@
+print(7 & 5)
+print(7 | 5)
+print(bin(7 & 5))   # 결과 자체가 이진수 형태로 출력됨
+
+# 1. 이진수로 변환
+# 2. 각 자리를 AND, OR 연산
+
+print(bin(0x4A3 | 25))
+
+secret_code = 1004
+print(7070 ^ secret_code)
+print(6258 ^ secret_code)
